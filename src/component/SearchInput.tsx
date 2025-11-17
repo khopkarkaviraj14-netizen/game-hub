7d51fe9257d1c7d058e6a3f74e80db6cbfac7968
Kaviraj Khopkar <khopkarkaviraj14@gmail.com>
2025-11-17 14:49:40 +0530

    Implement searching

--- src/component/SearchInput.tsx
+++ src/component/SearchInput.tsx
@@ -4,7 +4,7 @@ import { BsSearch } from 'react-icons/bs'
 const SearchInput = () => {
   return (
     <InputGroup>
-    <InputLeftElement children={<BsSearch />} />
+      <InputLeftElement children={<BsSearch />} />
     <Input borderRadius={20} placeholder='Search games...' variant='filled' />
     </InputGroup>
    )
